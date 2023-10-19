036d3e848833bf2dc60eda21be368da733e2b501
Laura Garcia Arch <garch.laura@gmail.com>
2023-10-19 12:16:47 +0200

    cpp

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:46:34 by laugarci          #+#    #+#             */
-/*   Updated: 2023/10/19 11:45:27 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/10/19 12:16:27 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void replace_words(std::string& str, const std::string& to_replace, const std::s
 		while (check != std::string::npos)
 		{
 			str.erase(check, to_replace.length());
+			str.insert(check, new_word);
 			check = str.find(to_replace);
 		}
 	}
@@ -35,9 +36,9 @@ void replace_words(std::string& str, const std::string& to_replace, const std::s
 int main(int ac, char **av)
 {
 	if (ac != 4)
-		return 1;
-	std::string line;
+		return (1);
 	std::ifstream infile(av[1]);
+	std::string outputname = std::string(av[1]) + ".replace";
 	std::string to_replace = av[2];
 	std::string new_word = av[3];
 	
@@ -46,10 +47,13 @@ int main(int ac, char **av)
 		std::string file_content((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
 		infile.close();
 		replace_words(file_content, to_replace, new_word);
+		std::ofstream outfile(outputname);
+		if (outfile.is_open())
+			outfile << file_content;
 	}
 	else
 	{
-		std::cout << "Error: No se pudo abrir el archivo." << std::endl;
+		std::cout << "Error, the file could not be opened or the file does not exist" << std::endl;
 		return (1);
 	}
 	return (0);
