650fe43ce64243b85f07c74418cc43e45a59ed0a
Laura Garcia Arch <garch.laura@gmail.com>
2023-12-09 18:07:47 +0100

    ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,29 +6,30 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 18:25:36 by laugarci          #+#    #+#             */
-/*   Updated: 2023/10/17 18:44:50 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/12/09 18:06:50 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include "Zombie.h"
 
-int main()
+int main(void)
 {
 	int	N;
 	int i;
 	Zombie *zombie;
 
-	std::string name = "Zombie";
-	N = 5;
-	zombie =  zombieHorde(N, name);
+	std::string name = "";
+	N = 1;
+	zombie = zombieHorde(N, name);
+	if (zombie == NULL)
+		return (1);
 	i = 0;
 	while (i < N)
-	{
+ 	{
 		zombie[i].announce();
 		i++;
 	}
-
 	delete[] zombie;
 	return (0);
 }
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 18:27:38 by laugarci          #+#    #+#             */
-/*   Updated: 2023/10/17 18:45:02 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/12/09 18:06:47 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,16 @@ Zombie* zombieHorde( int N, std::string name )
 	Zombie	*zombie;
 	int		i;
 	
+	if (N < 1)
+	{
+		std::cout << "One or more zombies are necessary." << std::endl;
+		return (NULL);
+	}
+	if (name.empty())
+	{
+		std::cout << "Name for zombies needed" << std::endl;
+		return (NULL);
+	}
 	zombie = new Zombie[N];
 	i = 0;
 	while (i < N)
