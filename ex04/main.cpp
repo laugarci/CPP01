865137f6bea8622ebb94cfb64966d13c3e255a7a
Laura Garcia Arch <garch.laura@gmail.com>
2023-12-14 12:48:32 +0100

    cpp01

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 15:07:57 by laugarci          #+#    #+#             */
-/*   Updated: 2023/12/13 15:59:04 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/12/14 12:48:14 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 bool	openFile(std::fstream &file, const char *name, std::ios_base::openmode mode)
 {
 	file.open(name, mode);
-	if (file.is_open() == false)
+	if (!file.is_open())
 	{
-		std::cerr << "Error opening file" << std::endl;
+		std::cerr << "Error opening this file: " << name << std::endl;
 		return (false);
 	}
 	return (true);
@@ -36,23 +36,44 @@ bool	check_arguments(int ac, char **av)
 		std::cerr << "Invalid arguments." << std::endl;
 		return (false);
 	}
+	return (true);
+}
+
+std::string	replaceWords(std::string content, std::string toReplace, std::string newWord)
+{
+	std::string newContent;
+	std::string	test;
+	int pos;
+
+	while (42)
+	{
+		pos = content.find(toReplace);
+		if (pos < 0)
+			break ;
+		newContent += content.substr(0, pos);
+		newContent += newWord;
+		content.erase(0, pos + toReplace.length());
+	}
+	newContent += content;
+	return (newContent);
 }
 
 int main(int ac, char **av)
 {
 	std::fstream	outfile;
+	std::fstream	infile;
 	std::string		outfileName;
+	std::string		content;
+	std::string		line;
 
-	if (check_arguments(ac, av))
+	if (check_arguments(ac, av) == false)
 		return (1);
-	if (ac != 4)
-	{
-		std::cout << "Usage: ./Sed <filename> <s1> <s2>" << std::endl;
+	outfileName = ((std::string)av[1]).append(".replace");
+	if (openFile(infile, av[1], std::ios_base::in) == false)
 		return (1);
-	}
-	outfileName = (std::string(av[1]) + ".replace").c_str();
-	if (openFile(av[1], av[1], std::ios_base::in))
+	if (openFile(outfile, outfileName.c_str(), std::ios_base::out) == false)
 		return (1);
-	if (openFile(outfile, outfileName, std::ios_base::out))
-		return (1);
+	while (std::getline(infile, line))
+		content += line.append(1, '\n');
+	outfile << replaceWords(content, av[2], av[3]);
 }
